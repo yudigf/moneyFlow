c4ff9203f0002828dfbcdd57bff3105ac9a8d986
yudigf <yudi.fitory@wgs-id.com>
2026-06-10 11:56:51 +0700

    feat: add Category model and migration

---
+++ backend/app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'type',
+    ];
+
+    /**
+     * Get all transactions belonging to this category.
+     */
+    public function transactions(): HasMany
+    {
+        return $this->hasMany(Transaction::class);
+    }
+}
