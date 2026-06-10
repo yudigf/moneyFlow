6b72cb942fc2cb9a5e53629883c7d742b6081efb
yudigf <yudi.fitory@wgs-id.com>
2026-06-10 15:04:30 +0700

    feat: setup API CORS on backend and Axios client on frontend

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
